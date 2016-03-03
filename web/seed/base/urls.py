055c889b972d21fa436c152cc418ed0284f5a4ec
Bogdan Staicu <bogdan.staicu@gmail.com>
2016-03-03 18:47:19 +0200

    redirect homepage to API page

--- web/seed/base/urls.py
+++ web/seed/base/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 from rest_framework_extensions.routers import ExtendedDefaultRouter
 
 from seed.blog.api import views as blog_api_views
@@ -13,6 +14,7 @@ router.register(r'tag', blog_api_views.TagViewSet, base_name='tag')
 router.register(r'post', blog_api_views.PostViewSet, base_name='post')
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/api/v1.0/')),
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1.0/', include(router.urls)),
     url(r'^api/search/posts/', PostSearchView.as_view(), name='search-posts'),
